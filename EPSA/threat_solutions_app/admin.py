40c8d167c95b8568b82285b084a0fd784fae3195
Jay Salmon <jaysal3@gmail.com>
2024-05-09 02:01:01 +0000

    Extended the solutions model.

--- EPSA/threat_solutions_app/admin.py
+++ EPSA/threat_solutions_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Threat, Safeguard, RiskTreatment
+from .models import Threat, ThreatMitigation, Safeguard, RiskTreatment, Vendor
 
 admin.site.register(Threat)
+admin.site.register(ThreatMitigation)
 admin.site.register(Safeguard)
 admin.site.register(RiskTreatment)
+admin.site.register(Vendor)
--- EPSA/threat_solutions_app/models.py
+++ EPSA/threat_solutions_app/models.py
@@ -7,6 +7,14 @@ class Threat(models.Model):
     def __str__(self):
         return self.name
 
+class ThreatMitigation(models.Model):
+    name = models.CharField(max_length=100)
+    threat = models.ForeignKey(Threat, on_delete=models.CASCADE, related_name='ThreatMitigation', null=True, blank=True)
+    vendor_description = models.TextField(blank=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
 
 class RiskTreatment(models.Model):
     name = models.CharField(max_length=100)
@@ -14,12 +22,20 @@ class RiskTreatment(models.Model):
     
     def __str__(self):
         return self.name
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
     
 class Safeguard(models.Model):
     name = models.CharField(max_length=100)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='safeguards_vendor', null=True, blank=True)
     description = models.TextField()
-    threats = models.ManyToManyField(Threat, related_name='safeguards', blank=True)
-    treatment = models.ForeignKey(RiskTreatment, on_delete=models.CASCADE, related_name='safeguards', null=True, blank=True)
+    threats = models.ManyToManyField(ThreatMitigation, related_name='safeguards_threats', blank=True)
+    treatment = models.ForeignKey(RiskTreatment, on_delete=models.CASCADE, related_name='safeguards_treatment', null=True, blank=True)
 
     def __str__(self):
         return self.name
