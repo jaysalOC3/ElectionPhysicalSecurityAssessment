e70d74bece3bf602cb456fe7070b837c2421f5d9
Jay Salmon <jaysal3@gmail.com>
2024-05-09 01:31:12 +0000

    Migrate Models to support more scalable solutions.

--- EPSA/threat_solutions_app/admin.py
+++ EPSA/threat_solutions_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Threat, Safeguard, RiskTreatment
 
-# Register your models here.
+admin.site.register(Threat)
+admin.site.register(Safeguard)
+admin.site.register(RiskTreatment)
--- EPSA/threat_solutions_app/models.py
+++ EPSA/threat_solutions_app/models.py
@@ -8,19 +8,18 @@ class Threat(models.Model):
         return self.name
 
 
-class Safeguard(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    threats = models.ManyToManyField(Threat, related_name='safeguards')
-
-    def __str__(self):
-        return self.name
-
-
 class RiskTreatment(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    safeguard = models.ForeignKey(Safeguard, on_delete=models.CASCADE, related_name='risk_treatments')
+    
+    def __str__(self):
+        return self.name
+    
+class Safeguard(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    threats = models.ManyToManyField(Threat, related_name='safeguards', blank=True)
+    treatment = models.ForeignKey(RiskTreatment, on_delete=models.CASCADE, related_name='safeguards', null=True, blank=True)
 
     def __str__(self):
         return self.name
